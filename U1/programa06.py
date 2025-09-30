340d06a763d44801b24e871fa8187b3151fd77b8
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-09-30 12:12:58 +0200

    Create programa06.py

---
+++ U1/programa06.py
@@ -0,0 +1,26 @@
+a = 10
+b = 3
+print(f"{a} + {b}: ", a + b)
+print(f"{a} - {b}: ", a - b)
+print(f"{a} * {b}: ", a * b)
+print(f"{a} / {b}: ", a / b)
+print(f"{a} // {b}: ", a // b)
+print(f"{a} % {b}: ", a % b)
+print(f"{a} ** {b}: ", a ** b)
+
+print()
+
+x = True
+y = False
+print(f"{x} AND {y}: ", x and y)
+print(f"{x} OR {y}: ", x or y)
+print(f"{x} NOT {y}: ", not x)
+
+print()
+
+print(f"{a} == {b}: ", a == b)
+print(f"{a} != {b}: ", a != b)
+print(f"{a} > {b}: ", a > b)
+print(f"{a} < {b}: ", a < b)
+print(f"{a} >= {b}: ", a >= b)
+print(f"{a} <= {b}: ", a <= b)
