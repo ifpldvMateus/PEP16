d300c2db6692895234f4c6c8ecdb95c76c0d456b
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-09-30 10:20:54 +0200

    Create programa02.py

---
+++ U1/programa02.py
@@ -0,0 +1,16 @@
+booleano = True
+numero1 = 10
+numero2 = 20
+decimal1 = 10.5
+decimal2 = 20.5
+cadena = "Buenos días"
+
+print(booleano, type(booleano))
+
+print(numero1, type(numero1))
+print(numero2, type(numero2))
+
+print(decimal1, type(decimal1))
+print(decimal2, type(decimal2))
+
+print(cadena, type(cadena))
