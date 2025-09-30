b4fe008911d6f851bbca58d1c09e6e12b4ecd067
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-09-30 12:28:09 +0200

    Create programa07.py

---
+++ U1/programa07.py
@@ -0,0 +1,7 @@
+print("34 en binario:", bin(34))
+print("34 en octal:", oct(34))
+print("34 en hexadecimal:", hex(34))
+
+print("0b1001 en decimal:", 0b1001)
+print("0o12 en decimal:", 0o12)
+print("0xa3f en decimal:", 0xa3f)
