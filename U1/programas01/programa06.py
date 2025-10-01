df9762d150c8e61c223abea7aa3755e807b81d2e
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-10-01 02:47:30 +0200

    Agrega el enunciado

--- U1/programas01/programa06.py
+++ U1/programas01/programa06.py
@@ -1,3 +1,10 @@
+'''
+Escribe un programa que use varias veces la función printf() para
+- Mostrar las operaciones del los operadores aritméticos de Python entre dos números
+- Mostrar las operaciones de los operadores lógicos de Python con valores booleanos
+- Mostrar las operaciones de los operadores de comparación de Python con valores booleanos y/o números
+'''
+
 a = 10
 b = 3
 print(f"{a} + {b}: ", a + b)
