a23970db060dff4a81e236ca98d289775b4d465c
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-10-01 02:41:06 +0200

    Update programa02.py
    Agrega el enunciado

--- U1/programas01/programa02.py
+++ U1/programas01/programa02.py
@@ -1,3 +1,7 @@
+'''
+Escribe un programa que escriba por pantalla: el booleano True, dos literales enteros, dos literales flotantes y una cadena de caracteres, y para cada uno de ellos su tipo de datos
+'''
+
 booleano = True
 numero1 = 10
 numero2 = 20
