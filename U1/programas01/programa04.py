f0132f4ff5655595dfa0da1c84c3fbe416a00e45
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-10-01 02:44:37 +0200

    Agrega el enunciado

--- U1/programas01/programa04.py
+++ U1/programas01/programa04.py
@@ -1,1 +1,10 @@
+'''
+Escribe un programa que muestre por pantalla el siguiente texto invocando solo una vez a la función print() con tres argumentos
+
+  Soy estudiante del
+  IES Leonardo Da Vini.
+  
+  Del ciclo DAW.
+'''
+
 print("Soy estudiante del\n", "IES Leonardo da Vinci.\n\n", "Del ciclo DAW.", sep="")
