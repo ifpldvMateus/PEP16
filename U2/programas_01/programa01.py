d758ed7d87c4a0f83e36a17bfa06c5f20bf63cfe
ifpldvMateus <mateus.leandro@educa.madrid.org>
2025-10-07 10:15:39 +0200

    Create programa01.py

---
+++ U2/programas_01/programa01.py
@@ -0,0 +1,9 @@
+'''
+Escribe un programa que pida primero un número par y luego un número impar (positivos o negativos). En caso de que uno o los dos valores no sea correcto (es decir no sea par o impar respectivamente), se mostrará un aviso
+'''
+
+par = int(input("Dame un número par: "))
+impar = int(input("Dame un número impar: "))
+
+if par % 2 != 0 or impar % 2 == 0:
+    print("Alguno de los valores no es correcto")
